303e9770d041baccf2bece0f1efe5a3371f3d1b7
atilanog <atilanog@utexas.edu>
2020-03-27 01:38:51 -0500

    Setup for Heroku Deployment

--- atilanogarciawebsite/settings.py
+++ atilanogarciawebsite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,5 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import django_heroku
+# Activate Django-Heroku
 django_heroku.settings(locals())
