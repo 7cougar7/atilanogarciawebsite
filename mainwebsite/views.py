610eb5d17920ae823026651129195b7e79bc43fc
atilanog <atilanog@utexas.edu>
2020-03-27 02:03:56 -0500

    Added .gitignore file

--- mainwebsite/views.py
+++ mainwebsite/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world. You're at the main landing page.")
+    return HttpResponse("Hello world. You're at the main landing page 123 Test :)")
 
 
