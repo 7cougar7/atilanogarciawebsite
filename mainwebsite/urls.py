3d183d78c0b514c6e0e0625c99b6ac1fa77cc84b
atilanog <atilanog@utexas.edu>
2020-04-11 21:12:50 -0500

    Generation functions improved

--- mainwebsite/urls.py
+++ mainwebsite/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('resume', views.resume, name='resume'),
     path('calendar', views.calendar_webpage, name='calendar_webpage'),
     path('rolls/<int:roll_size>', views.dnd_rolls, name='dnd_rolls'),
-
+    path('rollsapi/<int:roll_size>', views.dnd_rolls_api, name='dnd_rolls_api'),
 ]
