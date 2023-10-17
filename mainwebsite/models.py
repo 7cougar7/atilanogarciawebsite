11ab5d3f3cecb9c330169da07bf89689446656f4
Tilo Garcia <tilogarcia1@gmail.com>
2023-10-16 19:18:55 -0500

    Phone Call Model

--- mainwebsite/models.py
+++ mainwebsite/models.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
 from django.utils.crypto import get_random_string
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 def unique_short_code():
@@ -15,4 +16,13 @@ class ShortenedUrl(models.Model):
     real_url = models.URLField()
 
 
+class PhoneCall(models.Model):
+    phone_number = PhoneNumberField(null=False, blank=False, unique=False)
+    language = models.CharField()
+    is_active = models.BooleanField(default=False)
+    
+
+
+
 admin.site.register(ShortenedUrl)
+admin.site.register(PhoneCall)
